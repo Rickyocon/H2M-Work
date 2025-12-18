ac4fe06422d4a44afc3ed7a88a110252351bd9f9
Ricky O'Connor <94875214+Rickyocon@users.noreply.github.com>
2025-12-18 10:01:14 -0500

    Update junkmailimportlistsAppendValueToOne.ps1 script

--- H2M News Automatic Picture downloads/JunkListAppend/junkmailimportlistsAppendValueToOne.ps1
+++ H2M News Automatic Picture downloads/JunkListAppend/junkmailimportlistsAppendValueToOne.ps1
@@ -1,4 +1,4 @@
-Copy-Item -Path "\\app-server\APPS\_Intune Applications\1 Source\PowerShell\SafeSenders\Device script to append safe sender domain instead of overwrite\junkmailimportlistsAppendValueToOneDetection.txt" -Destination "C:\Support\SafeSenders\junkmailimportlistsAppendValueToOneDetection.txt" -Force
+Copy-Item -Path "\\app-server\APPS\_Intune Applications\1 Source\PowerShell\SafeSenders\JunkAppend\junkmailimportlistsAppendValueToOneDetection.txt" -Destination "C:\Support\SafeSenders\junkmailimportlistsAppendValueToOneDetection.txt" -Force
 
 # 1. Map the HKEY_USERS hive so PowerShell can see it
 New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS -ErrorAction SilentlyContinue
@@ -13,3 +13,4 @@ $Path = "HKU:\$sid\Software\Policies\Microsoft\Office\16.0\outlook\options\mail"
 # 4. Create the key and value with System-level permissions
 if (-not (Test-Path $Path)) { New-Item -Path $Path -Force }
 Set-ItemProperty -Path $Path -Name "junkmailimportlists" -Value 1 -Type DWord
+
